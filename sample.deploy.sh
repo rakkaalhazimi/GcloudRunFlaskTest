99612fdc01e5388f54059b76ac4dea8ae17ae8fe
rakka <rakkakeren@gmail.com>
2023-06-08 11:28:27 +0700

    rm: service account in run deploy args

--- sample.deploy.sh
+++ sample.deploy.sh
@@ -1,9 +1,8 @@
 gcloud builds submit \
   --tag gcr.io/$GOOGLE_CLOUD_PROJECT/flask-mount
 
-gcloud run deploy filesystem-app --source . \
+gcloud run deploy flask-mount \
     --image gcr.io/$GOOGLE_CLOUD_PROJECT/flask-mount \
     --execution-environment gen2 \
     --allow-unauthenticated \
-    --service-account fs-identity \
     --update-env-vars BUCKET=bucket_name
